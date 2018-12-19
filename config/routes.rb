3e2e470ed4276d174a3113db4354ad51b52714b1
Peter Kreppein <pmkreppein@gmail.com>
2018-12-19 06:17:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :author only: [:show, :new, :edit, :create, :update]
 end
