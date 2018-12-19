156af470807f0171453dcacfff1f4b2d905af88e
Peter Kreppein <pmkreppein@gmail.com>
2018-12-19 06:29:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :author, only: [:show, :new, :edit, :create, :update]
+  resources :author and resources :authors, only: [:show, :new, :edit, :create, :update]
   resources :post, only: [:show, :new, :edit, :create, :update]
 end
